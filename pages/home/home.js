b57d81de9c1e5f2fc907cfbc18d68d152874ad55
fairy <1648997108@qq.com>
2020-04-13 18:23:55 +0800

    8-网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 发送网络请求-1-原生方式
+  //  this.get_request_orign();
+   // 发送网络请求-2-promise封装方式
+  request({
+    url:"https://httpbin.org/post",
+  }).then(res=>console.log(res)).catch(err=>console.log(err))
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  get_request_orign(){
+    wx.request({
+      url: 'https://httpbin.org/get',
+      method:'post',
+      data:{
+        type:'sell',
+        page:1
+      },
+      success:(res)=>{
+        console.log("回调成功");
+        console.log(res);
+      },
+      fail:function(err){
+        console.log(err)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
